cca901eac1d267b4ae5c02fde1f4e566598885c8
abdulbosittojibayev <abdulbositsmall@gmail.com>
2023-10-06 19:38:23 +0500

    hello

--- script.js
+++ script.js
@@ -19,22 +19,42 @@ navbarMenu.addEventListener("click", toggleNavbarVisibility);
 let preveiwContainer = document.querySelector('.products-preview');
 let previewBox = preveiwContainer.querySelectorAll('.preview');
 
-document.querySelectorAll('.products-container .product').forEach(product =>{
-  product.onclick = () =>{
+document.querySelectorAll('.products-container .product').forEach(product => {
+  product.onclick = () => {
     preveiwContainer.style.display = 'flex';
     let name = product.getAttribute('data-name');
-    previewBox.forEach(preview =>{
+    previewBox.forEach(preview => {
       let target = preview.getAttribute('data-target');
-      if(name == target){
+      if (name == target) {
         preview.classList.add('active');
       }
     });
   };
 });
 
-previewBox.forEach(close =>{
-  close.querySelector('.fa-times').onclick = () =>{
+previewBox.forEach(close => {
+  close.querySelector('.fa-times').onclick = () => {
     close.classList.remove('active');
     preveiwContainer.style.display = 'none';
   };
 });
+
+// Swiper 
+var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
+const navList = document.querySelector(".L_I ul")
+const burger = document.querySelector(".checkbtn")
+
+burger.addEventListener("click", () => {
+  navList.classList.toggle("active")
+})
+
+let kotsan = false;
+
+console.log("Islomjon", kotsan = true);
+console.log("Dovudhon", kotsan = false);
